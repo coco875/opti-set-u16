f80cadc062a0e8b018ae5195f651a51d182fdb3e
coco875 <59367621+coco875@users.noreply.github.com>
2026-04-03 21:09:56 +0200

    Update register.rs

--- src/register.rs
+++ src/register.rs
@@ -8,6 +8,19 @@ macro_rules! register_set_int {
 #[macro_export]
 macro_rules! register_scenario {
     ($callback:ident, $($args:tt)*) => {
-        $callback!($($args)*, [InsertScenario, ContainsScenario, RemoveScenario, MixedScenario, SparseScenario, ClearScenario, LenScenario, IterScenario, UnionScenario, IntersectionScenario, DifferenceScenario, SymmetricDifferenceScenario])
+        $callback!($($args)*, [
+            InsertScenario,
+            ContainsScenario,
+            RemoveScenario,
+            MixedScenario,
+            SparseScenario,
+            ClearScenario,
+            LenScenario,
+            IterScenario,
+            UnionScenario,
+            IntersectionScenario,
+            DifferenceScenario,
+            SymmetricDifferenceScenario
+        ])
     };
 }
