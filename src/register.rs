48c919691204cf72142d4e6953ca930b4b5fe31d
lucas lefebvre <lucas.lefebvre046@gmail.com>
2026-05-23 22:26:25 +0200

    ajout custom_bst

--- src/register.rs
+++ src/register.rs
@@ -26,7 +26,8 @@ macro_rules! register_set_int {
             StdVecDicotomie,
             StdLinkedList,
             LibAvlTree,
-            LibRBTree
+            LibRBTree,
+            BinarySearchTree
         ])
     };
 }
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -23,6 +23,7 @@ mod std_tree_set;
 mod std_tree_set_default_func;
 mod std_vec;
 mod std_vec_dicotomie;
+mod custom_bst;
 
 pub use bit_tree_set::BitTreeSet;
 pub use byte_array_set::ByteArraySet;
@@ -49,6 +50,7 @@ pub use std_tree_set::StdTreeSet;
 pub use std_tree_set_default_func::StdTreeSetDefaultFunc;
 pub use std_vec::StdVec;
 pub use std_vec_dicotomie::StdVecDicotomie;
+pub use custom_bst::BinarySearchTree;
 
 pub trait SetInt: 'static {
     fn clear(&mut self);
